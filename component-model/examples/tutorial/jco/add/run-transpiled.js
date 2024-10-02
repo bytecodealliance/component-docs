9ad3cc336efdb5755010650b3aaec4927e74a406
Victor Adossi <vadossi@cosmonic.com>
2024-10-02 23:51:38 +0900

    fix: remove examples that should be in jco project
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- component-model/examples/tutorial/jco/add/run-transpiled.js
+++
@@ -1,4 +0,0 @@
-// If this import listed below is missing, please run `npm run transpile`
-import { add } from "./dist/transpiled/add.mjs";
-
-console.log("1 + 2 = " + add(1, 2));
--- component-model/examples/tutorial/jco/string-reverse/run-transpiled.js
+++
@@ -1,6 +0,0 @@
-// If this import listed below is missing, please run `npm run transpile`
-import { reverse } from "./dist/transpiled/reverser.mjs";
-
-const reversed = reverse.reverseString("!dlroW olleH");
-
-console.log(`reverseString('!dlroW olleH') = ${reversed}`);

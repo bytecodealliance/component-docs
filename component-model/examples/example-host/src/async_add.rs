04027d2336990f27c074afa7695a10749d870c74
Victor Adossi <vadossi@cosmonic.com>
2025-01-16 23:41:58 +0900

    fix(rust): use adder class for example host
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- component-model/examples/example-host/src/async_add.rs
+++ component-model/examples/example-host/src/async_add.rs
@@ -1,35 +1,61 @@
-use anyhow::Context;
 use std::path::PathBuf;
-use wasmtime::component::*;
+
+use anyhow::Context;
+use wasmtime::component::{Component, Linker};
 use wasmtime::{Config, Engine, Store};
+
 use crate::state::States;
 
-bindgen!({
-    path: "add.wit",
-    world: "example",
-    async: true
-});
+mod bindings {
+    //! Generated code for the
+    wasmtime::component::bindgen!({
+        path: "add.wit",
+        world: "adder",
+        async: true
+    });
+}
 
-pub async fn add(path: PathBuf, x: i32, y: i32) -> wasmtime::Result<i32> {
+/// Perform the add operation for a given WebAssembly component
+///
+/// This operation asynchronously (as opposed to synchronously
+/// without an async runtime like `tokio`).
+///
+/// # Arguments
+///
+/// * `path` - Path to the Wasm component bytes
+/// * `x` - The left hand side of the addition
+/// * `y` - The right hand side of the addition
+///
+pub async fn add(path: PathBuf, x: u32, y: u32) -> wasmtime::Result<u32> {
     // Construct engine
     let mut config = Config::default();
     config.async_support(true);
     let engine = Engine::new(&config)?;
+
     // Construct component
     let component = Component::from_file(&engine, path).context("Component file not found")?;
+
     // Construct store for storing running states of the component
     let wasi_view = States::new();
     let mut store = Store::new(&engine, wasi_view);
+
     // Construct linker for linking interfaces.
-    // For this simple adder component, no need to manually link additional interfaces.
     let mut linker = Linker::new(&engine);
-    // Add wasi exports to linker to support io interfaces
+
+    // Add wasi exports to linker to support I/O (as in `wasi:io`) interfaces
+    // see: https://github.com/WebAssembly/wasi-io
     wasmtime_wasi::add_to_linker_async(&mut linker)?;
-    let instance = Example::instantiate_async(&mut store, &component, &linker)
+
+    // Instantiate the component as an instance of the `adder` world,
+    // with the generated bindings
+    let instance = bindings::Adder::instantiate_async(&mut store, &component, &linker)
         .await
         .context("Failed to instantiate the example world")?;
+
+    // Call the add function on instance
     instance
+        .docs_adder_add()
         .call_add(&mut store, x, y)
         .await
-        .context("Failed to call add function")
+        .context("calling add function")
 }
--- component-model/examples/example-host/src/sync_add.rs
+++ component-model/examples/example-host/src/sync_add.rs
@@ -1,32 +1,57 @@
-use crate::state::States;
-use anyhow::Context;
 use std::path::PathBuf;
-use wasmtime::component::{bindgen, Component, Linker};
+
+use anyhow::Context;
+use wasmtime::component::{Component, Linker};
 use wasmtime::{Engine, Store};
 use wasmtime_wasi;
 
-bindgen!({
-    path: "add.wit",
-    world: "example",
-    async: false
-});
+use crate::state::States;
 
-pub fn add(path: PathBuf, x: i32, y: i32) -> wasmtime::Result<i32> {
+mod bindings {
+    wasmtime::component::bindgen!({
+        path: "add.wit",
+        world: "adder",
+        async: false
+    });
+}
+
+/// Perform a add operation for a given WebAssembly component
+///
+/// This operation happens synchronously (as opposed to asynchronously
+/// powered by an async runtime like `tokio`).
+///
+/// # Arguments
+///
+/// * `path` - Path to the Wasm component bytes
+/// * `x` - The left hand side of the addition
+/// * `y` - The right hand side of the addition
+///
+pub fn add(path: PathBuf, x: u32, y: u32) -> wasmtime::Result<u32> {
     // Construct engine
     let engine = Engine::default();
+
     // Construct component
     let component = Component::from_file(&engine, path).context("Component file not found")?;
+
     // Construct store for storing running states of the component
     let wasi_view = States::new();
     let mut store = Store::new(&engine, wasi_view);
+
     // Construct linker for linking interfaces.
-    // For this simple adder component, no need to manually link additional interfaces.
     let mut linker = Linker::new(&engine);
-    // Add wasi exports to linker to support io interfaces
+
+    // Add wasi exports to linker to support I/O (as in `wasi:io`) interfaces
+    // see: https://github.com/WebAssembly/wasi-io
     wasmtime_wasi::add_to_linker_sync(&mut linker).expect("Could not add wasi to linker");
-    let instance = Example::instantiate(&mut store, &component, &linker)
+
+    // Instantiate the component as an instance of the `adder` world,
+    // with the generated bindings
+    let instance = bindings::Adder::instantiate(&mut store, &component, &linker)
         .context("Failed to instantiate the example world")?;
+
+    // Call the add function on instance
     instance
+        .docs_adder_add()
         .call_add(&mut store, x, y)
-        .context("Failed to call add function")
+        .context("calling add function")
 }

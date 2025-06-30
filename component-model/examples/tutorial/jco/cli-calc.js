32f4402530f6193a033b9cf6a6c413235120e354
Victor Adossi <vadossi@cosmonic.com>
2025-06-30 19:36:37 +0900

    chore(lang/js): make jco tutorial easier to run
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- component-model/examples/tutorial/jco/cli-calc.js
+++ component-model/examples/tutorial/jco/cli-calc.js
@@ -1,4 +1,4 @@
 // See the README for details on *generation* of the required import
-import { calculate } from "./bindings/composed.js";
+import { calculate } from "./bindings/calculator.composed.js";
 
 console.log("Answer (to life) = " + calculate.evalExpression("add", 21, 21));

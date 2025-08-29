43773eb1bbd3f1b038d1bd14c0131b3343d352c5
Victor Adossi <vadossi@cosmonic.com>
2025-08-29 20:46:15 +0900

    fix(lang/rust): externalize code listings

--- component-model/examples/tutorial/adder/src/lib.rs
+++ component-model/examples/tutorial/adder/src/lib.rs
@@ -1,21 +1,27 @@
-#[allow(warnings)]
-mod bindings;
+mod bindings {
+    //! This module contains generated code for implementing
+    //! the `adder` world in `wit/world.wit`.
+    //!
+    //! The `path` option is actually not required,
+    //! as by default `wit_bindgen::generate` will look
+    //! for a top-level `wit` directory and use the files
+    //! (and interfaces/worlds) there-in.
+    wit_bindgen::generate!({
+        path: "wit/world.wit",
+    });
 
-// The comments that follow the `use` declaration below
-// correlate the rust module path segments with their
-// `world.wit` counterparts:
-use bindings::exports::docs::adder::add::Guest;
-//            <- items bundled with `export` keyword
-//                     <- package namespace
-//                           <- package
-//                                  <- interface name
+    use super::AdderComponent;
+    export!(AdderComponent);
+}
 
-struct Component;
+/// Struct off of which the implementation will hang
+///
+/// The name of this struct is not significant.
+#[allow(dead_code)]
+struct AdderComponent;
 
-impl Guest for Component {
+impl bindings::exports::docs::adder::add::Guest for AdderComponent {
     fn add(x: u32, y: u32) -> u32 {
         x + y
     }
 }
-
-bindings::export!(Component with_types_in bindings);
--- component-model/src/language-support/rust.md
+++ component-model/src/language-support/rust.md
@@ -141,33 +141,7 @@ We can fill in functionality of the component by implementing `bindings::Guest` 
 Your code should look something like the following:
 
 ```rs
-mod bindings {
-    //! This module contains generated code for implementing
-    //! the `adder` world in `wit/world.wit`.
-    //!
-    //! The `path` option is actually not required,
-    //! as by default `wit_bindgen::generate` will look
-    //! for a top-level `wit` directory and use the files
-    //! (and interfaces/worlds) there-in.
-    wit_bindgen::generate!({
-        path: "wit/world.wit",
-    });
-
-    use super::AdderComponent;
-    export!(AdderComponent);
-}
-
-/// Struct off of which the implementation will hang
-///
-/// The name of this struct is not significant.
-#[allow(dead_code)]
-struct AdderComponent;
-
-impl bindings::exports::docs::adder::add::Guest for AdderComponent {
-    fn add(x: u32, y: u32) -> u32 {
-        x + y
-    }
-}
+{{#include ../../examples/tutorial/adder/src/lib.rs}}
 ```
 
 There are a few points of note in the code listing above:
